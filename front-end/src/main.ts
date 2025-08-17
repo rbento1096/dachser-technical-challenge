70501cde55c87876e96275b94077e020e572a18f
Ricardo Bento <61906364+rbento1096@users.noreply.github.com>
2025-08-17 17:05:18 +0100

    chore: added angular material

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideAnimations(),
+    importProvidersFrom(HttpClientModule),
+  ],
+}).catch((err) => console.error(err));
